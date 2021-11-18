c7184a19165c49cb860cd169568fb8e39e031f1d
codingq007 <56585290+codingq007@users.noreply.github.com>
2021-11-18 17:18:43 +0900

    7일차

---
+++ 11_09_python_flask_/web_app/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask, session, Blueprint
+from flask import request, render_template,redirect,url_for, abort, flash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from datetime import datetime, timedelta
+from functools import wraps
+import time, random
+import math, os
+
+app = Flask(__name__)
+# webtest는 데이터베이스명
+app.config["MONGO_URI"] = "mongodb://localhost:27017/webtest"
+
+# flash를 사용하게 되면 SECRET_KEY를 사용해야 한다.
+app.config["SECRET_KEY"] = "test1234" # 노출되면 안되틑 암호화된키
+mongo = PyMongo(app)
+
+BOARD_IMAGE_PATH = "C:\\flask\\images"
+BOARD_ATTACH_FILE_PATH = "C:\\flask\\uploads"
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'gif', 'jpeg'])
+
+app.config["BOARD_IMAGE_PATH"] = BOARD_IMAGE_PATH
+app.config["BOARD_ATTACH_FILE_PATH"] = BOARD_ATTACH_FILE_PATH
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+
+if not os.path.exists(app.config["BOARD_IMAGE_PATH"]):
+    os.mkdir(app.config["BOARD_IMAGE_PATH"])
+
+if not os.path.exists(app.config["BOARD_ATTACH_FILE_PATH"]):
+    os.mkdir(app.config["BOARD_ATTACH_FILE_PATH"])
+
+
+
+# 세션의 유지시간은 환경변수에 설정한다.
+# 설정 시에는 timedelta()함수를 이용해서 설정한다. 
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes = 30)
+# app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(seconds = 5)
+
+from .common import login_required
+from .filter import datetime_format
+
+# web_app 패키지에 각 모듈을 귀속시킨다.
+from . import board
+from . import member
+
+app.register_blueprint(board.bp)
+app.register_blueprint(member.bp)
---
+++ 11_09_python_flask_/web_app/filter.py
@@ -0,0 +1,32 @@
+from web_app import app, datetime, time
+
+
+@app.template_filter('datetime_format')
+def datetime_format(value):
+    if value is None:
+        return ""
+
+    # 클라이언트의 현재 시스템의 local 타임
+    # 클라이언트의 local 타임을 datetime형식으로 만들어서 표현
+    # 게시글을 보는 사람이 있는 지역의 시간(현재 컴퓨터의 시간)
+    now_timestamp = time.time()
+    # print("현재 로컬 타임 :", now_timestamp)
+
+    # datetime 객체에 fromtimestamp, utcfromtimestamp 함수가 있다.
+
+    # 클라이언트의 시간을 기준으로 datetime 객체를 만듦
+    # print(datetime.fromtimestamp(now_timestamp))
+
+    # utcfromstamp는 UTC datetime 을 반환
+    # db에 저장된 UTC format과 같은 형태로 반환됨
+    # print(datetime.utcfromtimestamp(now_timestamp))
+
+    # 시간차 
+    offset_time = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    # print("offset_time : ", offset_time)
+
+    # db에 저장된 UTC 타임을 다시 현재의 로컬타임으로 변환해줌
+    value = datetime.fromtimestamp((int(value) / 1000)) + offset_time
+
+    # strftime() : format을 변경하는 함수
+    return value.strftime('%Y-%m-%d %H:%M:%S')
